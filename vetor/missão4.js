e38b89d374d8de52252fe1f15abd72b3e31bcd27
dev-arthursr <arthurdossantosrocha2007@gmail.com>
2026-04-06 21:00:15 -0300

    atividade parte 4

--- vetor/missão4.js
+++ vetor/missão4.js
@@ -1,16 +1,56 @@
-const prompt = require("prompt-sync")();
+const prompt = require('prompt-sync')();
 let opcao = '';
+let nome=[]
+let numero= []
 
-while (opcao !== "3"){
+
+
+
+while (opcao !== 3){
 console.log("\n =========MENU=======");
 console.log("1. cadastrar aluno");
 console.log("2. listar alunos");
 console.log("3. sair");
-opcao = prompt("opçâo:");
+opcao = prompt("opcao:");
+
+
+
+if (opcao === "1") {
+     const nomes = String(prompt(`Digite seu nome :`));
+    const numeros = Number(prompt('digitar sua nota:'));
+    
+    nome.push(nomes);
+    numero.push(numeros)
+     
+ }
+
+else if (opcao === "2"){
+    console.log("----lista dos alunos----")
+
+    for(let i=0; i <nome.length; i++){
+
+console.log(`${nome[i]} || nota ${numero[i]} || aluno: ${numero[i]>=7 ? "aprovado" : "reprovado"}`)
 
 }
+}
+else if (opcao === "3"){
+    console.log("vc saiu do menu.");
+    break;
+}
 
-for (let i = 0; i < 3; i++) {
-    let nome = prompt(`Digite seu nome ${i + 1}:`);
+else{
+    console.log("   0   0")
+    console.log("--______--")
 
+    console.log(" ======================")
+
+console.log(`
+ [:::::::]
+ | 0   0 |
+ |   ^   |
+ | \\___/ |
+ [_______]
+`);
+console.log("Não tem essa opção :(")
+}
 }
