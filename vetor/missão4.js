7b9d4d718cbefe587378ec925c3b8ff90db6d4a3
dev-arthursr <arthurdossantosrocha2007@gmail.com>
2026-04-09 21:07:41 -0300

    Add files via upload

--- vetor/missão4.js
+++ vetor/missão4.js
@@ -5,19 +5,18 @@ let numero= []
 
 
 
-
 while (opcao !== 3){
-console.log("\n =========MENU=======");
-console.log("1. cadastrar aluno");
-console.log("2. listar alunos");
-console.log("3. sair");
-opcao = prompt("opcao:");
+        console.log("\n =========MENU=======");
+        console.log("1. cadastrar aluno");
+        console.log("2. listar alunos");
+        console.log("3. sair");
+        opcao = prompt("opcao:");
 
 
 
 if (opcao === "1") {
      const nomes = String(prompt(`Digite seu nome :`));
-    const numeros = Number(prompt('digitar sua nota:'));
+    const numeros = Number(prompt('digitar sua nota: '));
     
     nome.push(nomes);
     numero.push(numeros)
@@ -25,8 +24,10 @@ if (opcao === "1") {
  }
 
 else if (opcao === "2"){
+    if(nome.length == 0){
+        console.log("Não existe nunhum aluno cadastrado!")
+    }
     console.log("----lista dos alunos----")
-
     for(let i=0; i <nome.length; i++){
 
 console.log(`${nome[i]} || nota ${numero[i]} || aluno: ${numero[i]>=7 ? "aprovado" : "reprovado"}`)
